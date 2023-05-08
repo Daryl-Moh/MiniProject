96c5011a27999c8439e855ca14dc7e14e911b9b2
Daryl_Moh <Daryl.Moh@hotmail.com>
2023-05-08 14:45:37 +0800

    redo of client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from './components/list.component';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar.component';
-import { SearchComponent } from './components/search.component';
-import { PortfolioComponent } from './components/portfolio.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListComponent } from './components/list/list.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  
+  { path: '', component: HomeComponent},
   { path: 'list', component: ListComponent},
   { path: 'search', component: SearchComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'about', component: AboutComponent},
   { path: 'portfolio', component: PortfolioComponent},
-  { path: "**", redirectTo: "", pathMatch: 'full' }
+  { path: "**", redirectTo: "/", pathMatch: 'full' }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListComponent } from './components/list/list.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar.component';
-import { ListComponent } from './components/list.component';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -13,8 +18,13 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterComponent,
     NavbarComponent,
-    ListComponent
+    ListComponent,
+    PortfolioComponent,
+    SearchComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
